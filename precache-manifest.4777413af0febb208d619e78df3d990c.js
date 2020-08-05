635c4dbc417f20b4959952a42355e500a656d4d0
Aalya <aalya.banu@gmail.com>
2020-08-05 17:03:32 +0100

    Updates

---
+++ precache-manifest.4777413af0febb208d619e78df3d990c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b2373be204d24198d3ebd84ff20ba2",
+    "url": "/socialSite/index.html"
+  },
+  {
+    "revision": "d50ae5160f86ffd1f5e1",
+    "url": "/socialSite/static/css/2.9acb0b1b.chunk.css"
+  },
+  {
+    "revision": "df1ea14f12dc11c35689",
+    "url": "/socialSite/static/css/main.4302fe4d.chunk.css"
+  },
+  {
+    "revision": "d50ae5160f86ffd1f5e1",
+    "url": "/socialSite/static/js/2.969441bd.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/socialSite/static/js/2.969441bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1ea14f12dc11c35689",
+    "url": "/socialSite/static/js/main.76e1cda4.chunk.js"
+  },
+  {
+    "revision": "01424228913c81b349db",
+    "url": "/socialSite/static/js/runtime-main.0e7e2922.js"
+  },
+  {
+    "revision": "20d6a460b4461b9e2b337945bf30e58b",
+    "url": "/socialSite/static/media/logo.20d6a460.png"
+  }
+]);
